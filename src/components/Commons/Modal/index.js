6b25d1d36e480dc6bbff0fdb07b9286d03c54a32
silvia carrasco <silvia.carrasco@igmweb.com>
2019-09-02 12:07:18 +0200

    orden button modal

--- src/components/Commons/Modal/index.js
+++ src/components/Commons/Modal/index.js
@@ -25,16 +25,19 @@ export default function _Modal({children, className, show, options, disableConfi
             </div>
             {options && (options.onOk || options.onCancel) ? <div className="modal-footer" style={{display: 'block'}}>
                 {options && options.footer && options.footer.textHtml ? <span className="float-left" dangerouslySetInnerHTML={{__html: options.footer.textHtml}}></span> : null}
-                {options && options.onCancel ?
-                    <button type="button" className={`float-right ml-1 btn btn-secondary ${options && options.btSize ? 'btn-' + options.btSize : ''}`} onClick={() => options.onCancel('CANCEL')}>
-                        {options && options.iconButton ?
-                            <FontAwesomeIcon className={"mr-1"} icon={faTimes}/> : null} {options && options.textCancel ? options.textCancel : 'Cancel'}
-                    </button> : null}
+
                 {options && options.onOk ?
                     <button type="button" disabled={disableConfirm || false} className={`float-right ml-1 btn btn-primary ${options && options.btSize ? 'btn-' + options.btSize : ''}`} onClick={() => options.onOk('OK')}>
                         {options && options.iconButton ?
                             <FontAwesomeIcon className={"mr-1"} icon={options.iconOK === 'ok' ? faCheck : (options.iconOK==='add' ? faPlus : (options.iconOK==='update' ? faSave : faCheck))}/> : null} {options && options.textOk ? options.textOk : 'Ok'}
                     </button> : null}
+
+                {options && options.onCancel ?
+                    <button type="button" className={`float-right ml-1 btn btn-secondary ${options && options.btSize ? 'btn-' + options.btSize : ''}`} onClick={() => options.onCancel('CANCEL')}>
+                        {options && options.iconButton ?
+                            <FontAwesomeIcon className={"mr-1"} icon={faTimes}/> : null} {options && options.textCancel ? options.textCancel : 'Cancel'}
+                    </button> : null}
+
             </div> : null}
         </Modal>
     )
